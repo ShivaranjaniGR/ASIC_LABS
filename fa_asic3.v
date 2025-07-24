71f4775f87c40d87765eae4c0ac0d3ab6ae88750
ShivaranjaniGR <147621524+ShivaranjaniGR@users.noreply.github.com>
2025-07-24 15:17:32 +0530

    Add files via upload

---
+++ fa_asic3.v
@@ -0,0 +1,20 @@
+module fa_asic3(cout,s,a,b,c,clk);
+input a,b,c,clk;
+output reg cout,s;
+reg a_1,b_1,c_in;
+wire s_1,c_1;
+always @(posedge clk)
+begin
+a_1 <= a;
+b_1 <=b;
+c_in <=c;
+end
+assign s_1 = a_1^b_1^c_in;
+assign c_1 = (a_1 & b_1)|(b_1 & c_in)|(a_1 & c_in);
+always @(posedge clk)
+begin
+s <= s_1;
+cout <= c_1;
+end
+endmodule
+
